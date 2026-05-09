c19808ee6a058a74a774deebce1e589a8b64e722
dariusp0p <dariuspop2005@gmail.com>
2026-05-09 16:54:18 +0300

    feat: updated the Server Entry Point to also include the ConversationList Service

--- ChatAndEvents/ChatAndEvents.API/ChatAndEvents.API.Server/Program.cs
+++ ChatAndEvents/ChatAndEvents.API/ChatAndEvents.API.Server/Program.cs
@@ -29,46 +29,36 @@ builder.Services.AddSwaggerGen(options =>
     // to prevent naming collisions!
     options.CustomSchemaIds(type => type.FullName);
 });
-//chat ->
+
+// chat module services
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IConversationListService, ConversationListService>();
 builder.Services.AddScoped<IProfileService, ProfileService>();
 builder.Services.AddScoped<ISearchService, SearchService>();
 builder.Services.AddScoped<IDirectMessageService, DirectMessageService>();
-
 builder.Services.AddScoped<IBlockService, BlockService>();
 builder.Services.AddScoped<IFriendListService, FriendListService>();
 builder.Services.AddScoped<IFriendRequestService, FriendRequestService>();
-
 builder.Services.AddScoped<IGroupService, GroupService>();
 builder.Services.AddScoped<IMemberPanelService, MemberPanelService>();
 builder.Services.AddScoped<IMentionService, MentionService>();
-
 builder.Services.AddScoped<IMessageInteractionService, MessageInteractionService>();
 builder.Services.AddScoped<IMessageService, MessageService>();
-
 builder.Services.AddScoped<IModerationService, ModerationService>();
 builder.Services.AddScoped<IReadReceiptService, ReadReceiptService>();
-//events ->
+
+// events module services
 builder.Services.AddScoped<IMemoryService, MemoryService>();
 builder.Services.AddScoped<IQuestApprovalService, QuestApprovalService>();
 builder.Services.AddScoped<IQuestService, QuestService>();
-
 builder.Services.AddScoped<INotificationService, NotificationService>();
-
 builder.Services.AddScoped<IEventStatisticsService, EventStatisticsService>();
-
 builder.Services.AddScoped<IEventService, EventService>();
-
 builder.Services.AddScoped<IDiscussionService, DiscussionService>();
-
 builder.Services.AddScoped<ICategoryServices, CategoryServices>();
-
 builder.Services.AddScoped<IAttendedEventService, AttendedEventService>();
-
 builder.Services.AddScoped<IAnnouncementService, AnnouncementService>();
-
 builder.Services.AddScoped<IAchievementService, AchievementService>();
-
 builder.Services.AddScoped<IReputationService, ReputationService>();
 
 var app = builder.Build();
